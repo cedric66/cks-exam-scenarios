064f9b2f46cd8cca0be2e21c5125c9ed0b465a9a
ronny.ardianto <ronny.ardianto@accelbyte.net>
2024-08-07 07:03:17 +0700

    fix: verify step1

--- seccomp-profile/step1/verify.sh
+++ seccomp-profile/step1/verify.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-# Check if the seccomp profile exists on the node
-if [ -f /var/lib/kubelet/seccomp/seccomp-profile.json ]; then
+# Copy the seccomp profile from the node to the local machine
+scp node01:/var/lib/kubelet/seccomp/seccomp-profile.json /tmp/seccomp-profile.json 2>/dev/null
+
+# Check if the seccomp profile was successfully copied
+if [ -e /tmp/seccomp-profile.json ]; then
+  # File exists, remove the copied file and exit with success status
+  rm /tmp/seccomp-profile.json
   exit 0
 else
+  # File does not exist, exit with failure status
   exit 1
 fi
