f460450b098b0ab998ebf6208d92daa8e31e956b
ronny.ardianto <ronny.ardianto@accelbyte.net>
2024-08-19 09:09:26 +0700

    fix: verify sh

--- network-policy-1/step1/verify.sh
+++ network-policy-1/step1/verify.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-res=$(kubectl get networkpolicy allow-specific-pods -n restricted)
+res=$(kubectl get networkpolicy allow-specific-pods -n restricted -o json)
 
-if [[ -n "$res" ]]; then
+if [[ $(echo "$res" | jq -r .spec.podSelector.matchLabels.app) == "commerce-frontend" ]]; then
+  exit 0
+else
+  exit 1
+fi
+
+if [[ $(echo "$res" | jq -r .spec.ingress[0].from[0].namespaceSelector.matchLabels.kubernetes.io/metadata.name) == "restricted" ]]; then
+  exit 0
+else
+  exit 1
+fi
+
+if [[ $(echo "$res" | jq -r .spec.ingress[0].from[0].podSelector.matchLabels.app) == "jumpbox" ]]; then
   exit 0
 else
   exit 1
