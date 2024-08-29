32f812574cb1af82cade9b47887f9df2cc7a7d24
ronny.ardianto <ronny.ardianto@accelbyte.net>
2024-08-29 08:00:35 +0700

    release: scenario container non-root

---
+++ container-non-root/step1/verify.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Verify that the Pod is running as a non-root user
+pod_status=$(kubectl get pod dark-illusion -n limited -o jsonpath='{.status.phase}')
+run_as=$(kubectl get pod dark-illusion -n limited -o jsonpath='{.spec.securityContext.runAsNonRoot}')
+
+if [[ "$pod_status" == "Running" && "$run_as" == "true" ]]; then
+  exit 0
+else
+  exit 1
+fi
+
+res=$(cat /opt/non-root/answer | grep "prime-ships")
+
+if [[ -n "$res" ]]; then
+  exit 0
+else
+  exit 1
+fi
