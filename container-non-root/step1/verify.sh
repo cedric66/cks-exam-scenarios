b5d1c8c891b188e2cbbec8f608d16b128634e09a
ronny.ardianto <ronny.ardianto@accelbyte.net>
2024-08-29 08:21:23 +0700

    fix: verify script

--- container-non-root/step1/verify.sh
+++ container-non-root/step1/verify.sh
@@ -3,17 +3,14 @@
 # Verify that the Pod is running as a non-root user
 pod_status=$(kubectl get pod dark-illusion -n limited -o jsonpath='{.status.phase}')
 run_as=$(kubectl get pod dark-illusion -n limited -o jsonpath='{.spec.containers[0].securityContext.runAsNonRoot}')
+res=$(cat /opt/non-root/answer)
 
 if [[ "$pod_status" == "Running" && "$run_as" == "true" ]]; then
-  exit 0
-else
-  exit 1
-fi
-
-res=$(cat /opt/non-root/answer | grep "prime-ships")
-
-if [[ -n "$res" ]]; then
-  exit 0
+  if [[ "$res" == "prime-ships" ]]; then
+    exit 0
+  else
+    exit 1
+  fi
 else
   exit 1
 fi
