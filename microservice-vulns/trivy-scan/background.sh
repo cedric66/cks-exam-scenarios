88e77d39530c78ba1d8827a50b5c177ba81b34b3
Joe Butler <12396580+ButlerJoseph@users.noreply.github.com>
2025-01-21 22:15:33 +0000

    Add foreground.sh and remove prefix from title

--- microservice-vulns/trivy-scan/background.sh
+++ microservice-vulns/trivy-scan/background.sh
@@ -3,3 +3,5 @@ wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dear
 echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb generic main" | sudo tee -a /etc/apt/sources.list.d/trivy.list
 sudo apt-get update
 sudo apt-get install trivy -y
+
+touch /tmp/finished
