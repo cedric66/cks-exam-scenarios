677b5b946cd17f7f97618d6e930063e174078314
Joe Butler <12396580+ButlerJoseph@users.noreply.github.com>
2025-01-21 22:06:19 +0000

    Update background and further fixes to index.json

--- microservice-vulns/trivy-scan/background.sh
+++ microservice-vulns/trivy-scan/background.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-apt-get update
-apt-get install -y wget apt-transport-https gnupg lsb-release
-wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | apt-key add -
-echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | tee -a /etc/apt/sources.list.d/trivy.list
-apt-get update
-apt-get install -y trivy
+curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sudo sh -s -- -b /usr/local/bin v0.58.0-61-gc00232720
